aa7c5f59617ab606173287da65f2c1682ee9aaa4
salexkidd <salexkidd@gmail.com>
2017-08-26 16:28:25 +0900

    add description

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # django-malice
 
+[![CircleCI](https://circleci.com/gh/salexkidd/django_malice.svg?style=svg)](https://circleci.com/gh/salexkidd/django_malice)
+
 django-malice is a simple Django app that provides an easy and simple View Error(403, 404, 500) and 200 OK Testing for develop and production environment.
 
 # Feature
--- setup.py
+++ setup.py
@@ -2,9 +2,26 @@ from setuptools import setup
 
 
 setup(
-    name='django_malice',
+    name='django-malice',
+    author = "salexkidd",
+    author_email = "salexkidd@gmail.com",
+    url = "https://github.com/salexkidd/django_malice/",
+    description='A simple Django app that enables own occuring error.',
+    long_description=README,
+    keywords = ["django", "testing",],
     version='0.1',
-    install_requires=[
-        "Django==1.11.1",
-    ]
+    packages=['malice'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
